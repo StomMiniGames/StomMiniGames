659c963143d01b320d34767d3e3876955c3926cf
Nucker <nuckermail@gmail.com>
2023-07-03 20:20:05 +0100

    Add polar dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,6 +21,7 @@ repositories {
 dependencies {
     implementation("dev.hollowcube:minestom-ce:54e839e58a")
     implementation("com.github.SimplyMerlin:FSMChamp:v1.1.0")
+    implementation("dev.hollowcube:polar:1.3.0")
 }
 
 
